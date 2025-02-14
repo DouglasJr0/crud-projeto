ae503eda517ec16892c7369ad76807ba3f0322e2
Douglasjr0 <juniormattos0610@gmail.com>
2025-02-14 08:48:55 -0300

    testando

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -216,7 +216,7 @@ $(document).ready(function () {
             data_nascimento: data_nascimento,
             profissao: profissao
         };
-
+ikjbjkhbvkjhv {}
         $.ajax({
             type: "POST",
             url: "/cadastrarUsuario",
